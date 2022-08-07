306880f2ee0685c935c4a57f5515fe845eeb8a38
NJAO <1157515428@qq.com>
2022-08-08 01:21:41 +0800

    created goods page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,34 +2,37 @@ import { Component } from 'react'
 import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
 
-interface props {
-  [key:string] : any
+type PageStateProps = {
+  myName:string
 }
+
+type PageDispatchProps = {
+  getRecommendMyName: () => string
+}
+
+type PageOwnProps = {}
+
+type PageState = {
+  myName:string
+}
+
+type IProps =  PageStateProps & PageDispatchProps & PageOwnProps;
 export default class Index extends Component {
-  state: Readonly<props> = {
-    myName:'sdsds'
-  }
   componentWillMount () { }
  
   componentDidMount () { }
 
   componentWillUnmount () { }
-  props: Readonly<props> = {
-    myName:'asdasdasd'
-  } 
+
   componentDidShow () { }
 
   componentDidHide () { }
-
-  logName = ():void => {
-    console.log(this.state.myName);
-  }
 
   render () {
     return (
       <View className='index'>
         <Text>Helsld!</Text>
-        <Button onClick={this.logName}>点击</Button>
+        <Button>点击</Button>
       </View>
     )
   }
